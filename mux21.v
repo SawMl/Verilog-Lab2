69de0a47797edfca5d65cd7acac9206b099842bc
SawMl <92833091+SawMl@users.noreply.github.com>
2021-10-28 11:23:41 -0700

    mux21

---
+++ mux21.v
@@ -0,0 +1,13 @@
+`timescale 1ns / 1ps
+
+
+module mux21(
+    input S,
+    input D0,
+    input D1,
+    output Y
+    );
+    
+    assign Y = (!S & D0) | (S & D1);
+    
+endmodule
---
+++ tb_mux21.v
@@ -0,0 +1,30 @@
+`timescale 1ns / 1ps
+
+module tb_mux21;
+    reg D0;
+    reg D1;
+    reg S;
+    wire Y;
+    
+    mux21 instant
+    (
+        .D0(D0),
+        .D1(D1),
+        .S(S),
+        .Y(Y)
+    );
+    
+    initial
+      begin
+        D0 = 1'b0;
+        D1 = 1'b1;
+        S = 1'b0;
+        #20;
+        
+        D0 = 1'b0;
+        D1 = 1'b1;
+        S = 1'b1;
+        #20;
+      end
+        
+endmodule
