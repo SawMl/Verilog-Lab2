9cd890e24e7de502df72ff5d3018423a2581ddba
SawMl <92833091+SawMl@users.noreply.github.com>
2021-11-04 13:44:54 -0700

    final

--- tb_FA4.v
+++ tb_FA4.v
@@ -7,31 +7,32 @@ module tb_FA4;
     reg [3:0] B;
     reg Cin;
     wire [3:0] Sum;
-    
+    wire Cout;
+
     FA4 instant(.A(A), .B(B), .Cin(Cin), .Cout(Cout), .Sum(Sum));
-    
+
     initial begin
-        
+
         A=4'b0110;
         B=4'b0100;
         Cin=1'b0;
         #20;
-        
+
          A=4'b1000;
         B=4'b1001;
         Cin=1'b1;
         #20;
-        
+
          A=4'b1110;
         B=4'b0010;
         Cin=1'b0;
         #20;
-        
+
         A=4'b1010;
         B=4'b1011;
         Cin=1'b0;
         #20;
-        
-    
+
+
     end
 endmodule
